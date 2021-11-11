ade9872481edb76a079ffc86790d3b9f832e92cf
Piotr Kozłowski <pitrx77@gmail.com>
2021-11-11 23:57:04 +0100

    making some small changes

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -6,36 +6,38 @@ import { ProductService } from './../product.service';
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.scss']
+  styleUrls: ['./product-page.component.scss'],
 })
 export class ProductPageComponent implements OnInit {
-
   @Input() product: Product;
   // @Output() currentId: EventEmitter<string> = new EventEmitter();
   products: Product[];
   id: string;
   idToNumber: number;
 
-
-  constructor(private route: ActivatedRoute, private ProductService: ProductService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private ProductService: ProductService
+  ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
-    this.id = this.id.replace(':', '')
+    this.id = this.id.replace(':', '');
     this.idToNumber = parseInt(this.id);
 
     this.getProduct();
   }
 
   getProduct(): void {
-    this.ProductService.getProducts().subscribe(products => this.products = products);
+    this.ProductService.getProducts().subscribe(
+      (products) => (this.products = products)
+    );
   }
 
   ChangeToInt(value) {
-    return Math.floor(value)
+    return Math.floor(value);
   }
   // setCurrentId() {
   //   this.currentId.emit(this.id)
   // }
-
 }
