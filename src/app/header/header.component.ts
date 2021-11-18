59c50389e7d52594ade1ea1970410e32ad51caf5
Piotr Kozłowski <pitrx77@gmail.com>
2021-11-18 23:59:20 +0100

    creating intial counter of products in the basket

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  productsInBasket$: Observable<string>;
+  numberOfProducts: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<{ message: string }>) {
+    this.productsInBasket$ = store.select('message');
   }
 
+  ngOnInit(): void {}
+
+  countItemsInBasket() {
+    this.productsInBasket$.subscribe(
+      (items) => (this.numberOfProducts = items)
+    );
+  }
 }
