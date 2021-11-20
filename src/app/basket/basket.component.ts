774424404db5591c3df1a2bd3255e054f5393b3d
Piotr Kozłowski <pitrx77@gmail.com>
2021-11-20 23:59:31 +0100

    sort of items in the basket

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -5,32 +5,60 @@ import { Product } from './../products';
 import { ProductService } from './../product.service';
 import { emptyBasket } from '../reducers/basketActions';
 
+interface ProductAndNumber {
+  amount: number;
+  productId: string;
+}
+
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
   styleUrls: ['./basket.component.scss'],
 })
 export class BasketComponent implements OnInit {
-  message$: Observable<string>;
+  productsInBasket$: Observable<string[]>;
+  arrayOfProducts: ProductAndNumber[] = [];
   @Input() product: Product;
 
   products: Product[];
 
   constructor(
-    private store: Store<{ message: string }>,
+    private store: Store<{ message: string[] }>,
     private ProductService: ProductService
   ) {
-    this.message$ = store.select('message');
+    this.productsInBasket$ = store.select('message');
   }
 
   ngOnInit(): void {
     this.getProduct();
+    this.productsInBasket$.subscribe(
+      (items) => (this.arrayOfProducts = this.countNumberOfItems(items))
+    );
   }
 
   getProduct(): void {
     this.ProductService.getProducts().subscribe(
       (products) => (this.products = products)
     );
+  }
+
+  countNumberOfItems(products: string[]): ProductAndNumber[] {
+    let tempArray: ProductAndNumber[] = [];
+    for (let i = 0; i < products.length; i++) {
+      let index = tempArray.findIndex(
+        (product) => product.productId === products[i]
+      );
+      if (index === -1) {
+        tempArray.push({ amount: 1, productId: products[i] });
+      } else {
+        tempArray[index] = {
+          ...tempArray[index],
+          amount: tempArray[index].amount + 1,
+        };
+      }
+    }
+
+    return tempArray;
   }
 
   ChangeToInt(value) {
